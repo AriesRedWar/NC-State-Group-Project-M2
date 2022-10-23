211adf94a977613a33951586df7627a7e8bb7b9a
shetara2022 <smithshetarall@gmail.com>
2022-10-23 18:47:03 -0400

    added addgame css

--- front-end-react/src/componets/game.js
+++ front-end-react/src/componets/game.js
@@ -1,10 +1,26 @@
+
+import Form from 'react-bootstrap/Form';
 const React = require("react");
 
-export default function Game () {
-    return(
-<div className="GamePage">
-<h1>Find the best games to play</h1>
-</div>
+export default function Game() {
+    return (
+        <>
+            <div className="GamePage">
+                <h1>Find the best games to play</h1>
+            </div>
+            <div>
+                <Form.Label>Rating</Form.Label>
+                <Form.Select defaultValue="Choose...">
+                    <option>Rate the Game</option>
+                    <option>⭐️</option>
+                    <option>⭐️⭐️</option>
+                    <option>⭐️⭐️⭐️</option>
+                    <option>⭐️⭐️⭐️⭐️</option>
+                    <option>⭐️⭐️⭐️⭐️⭐️</option>
+                </Form.Select>
+
+            </div>
+        </>
     )
 
 }
