536c204d7a77c817ed5f97b30c7f80f05b91e120
AriesRedWar <philipjbowman5@gmail.com>
2022-11-01 18:56:00 -0400

    deployment working for Build react changes with instructor

--- front-end-react/src/componets/ShowGame.js
+++ front-end-react/src/componets/ShowGame.js
@@ -3,15 +3,18 @@ import { useState, useEffect } from "react";
 import "./css/viewgame.css";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 
 function ViewsShow() {
   let { id } = useParams();
   const navigate = useNavigate();
-  console.log(id);
+  console.log('THIS IS OUR id!~!! beore use effect', id);
 
   const [state, setState] = useState({});
+
   const getData = async () => {
+    console.log('THi si our id!!!', id)
     // console.log("We got clicked", id);
     const data = await fetch("/games/" + id);
     // console.log("DATA inital from backed", data);
@@ -43,26 +46,27 @@ function ViewsShow() {
 
   return (
     <div>
-      <div key={id} className="container">
+      <h1> View page</h1>
+      {/* <div key={id} className="container">
         <h1>{state.gamename}</h1>
         <img className="placeimg2" src={state.pic} alt={state.gamename} />
         <h4>Genre: {state.genre}</h4>
         <p> Description: {state.description}</p>
         <div className="buttons">
           <form className="edit" id="btn" method="POST">
-            <a
+            <Link
               type="click"
-              href={`/games/${state._id}/edit`}
+              to={`/games/${state._id}/edit`}
               className="btn btn-warning"
             >
               Edit
-            </a>
+            </Link>
           </form>
           <button onClick={deleteGame} className="btn btn-danger">
             Delete
           </button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
